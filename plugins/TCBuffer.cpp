19ee2186c1576a776f77c7c99554429aba474f3b
Philip Rodrigues <rodriges@fnal.gov>
2022-04-20 14:47:48 +0200

    Add TCBuffer

---
+++ plugins/TCBuffer.cpp
@@ -0,0 +1,127 @@
+/**
+ * @file TCBuffer.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2021.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "TCBuffer.hpp"
+
+#include "daqdataformats/GeoID.hpp"
+
+#include <string>
+
+namespace dunedaq {
+namespace trigger {
+
+TCBuffer::TCBuffer(const std::string& name)
+  : DAQModule(name)
+  , m_thread(std::bind(&TCBuffer::do_work, this, std::placeholders::_1))
+  , m_queue_timeout(100)
+{
+
+  register_command("conf", &TCBuffer::do_conf);
+  register_command("start", &TCBuffer::do_start);
+  register_command("stop", &TCBuffer::do_stop);
+  register_command("scrap", &TCBuffer::do_scrap);
+}
+
+void
+TCBuffer::init(const nlohmann::json& init_data)
+{
+  try {
+    m_input_queue_tcs.reset(new tcs_source_t(appfwk::queue_inst(init_data, "tc_source")));
+    m_input_queue_dr.reset(new dr_source_t(appfwk::queue_inst(init_data, "data_request_source")));
+    m_output_queue_frag.reset(new fragment_sink_t(appfwk::queue_inst(init_data, "fragment_sink")));
+  } catch (const ers::Issue& excpt) {
+    throw dunedaq::trigger::InvalidQueueFatalError(ERS_HERE, get_name(), "input/output", excpt);
+  }
+  m_error_registry = std::make_unique<readoutlibs::FrameErrorRegistry>();
+  m_latency_buffer_impl = std::make_unique<latency_buffer_t>();
+  m_request_handler_impl = std::make_unique<request_handler_t>(m_latency_buffer_impl, m_error_registry);
+  m_request_handler_impl->init(init_data);
+}
+
+void
+TCBuffer::get_info(opmonlib::InfoCollector& /* ci */, int /*level*/)
+{
+}
+
+void
+TCBuffer::do_conf(const nlohmann::json& args)
+{
+  // Configure the latency buffer before the request handler so the request handler can check for alignment
+  // restrictions
+
+  m_latency_buffer_impl->conf(args);
+  m_request_handler_impl->conf(args);
+
+  TLOG_DEBUG(2) << get_name() + " configured.";
+}
+
+void
+TCBuffer::do_start(const nlohmann::json& args)
+{
+  m_request_handler_impl->start(args);
+  m_thread.start_working_thread("tcbuffer");
+  TLOG_DEBUG(2) << get_name() + " successfully started.";
+}
+
+void
+TCBuffer::do_stop(const nlohmann::json& args)
+{
+  m_thread.stop_working_thread();
+  m_request_handler_impl->stop(args);
+  m_latency_buffer_impl->flush();
+  TLOG_DEBUG(2) << get_name() + " successfully stopped.";
+}
+
+void
+TCBuffer::do_scrap(const nlohmann::json& args)
+{
+    m_request_handler_impl->scrap(args);
+    m_latency_buffer_impl->scrap(args);
+}
+
+void
+TCBuffer::do_work(std::atomic<bool>& running_flag)
+{
+  size_t n_tcs_received = 0;
+  size_t n_requests_received = 0;
+  
+  while (running_flag.load()) {
+    
+    bool popped_anything=false;
+    
+    try {
+      triggeralgs::TriggerCandidate tc;
+      m_input_queue_tcs->pop(tc);
+      popped_anything = true;
+      m_latency_buffer_impl->write(TCWrapper(tc));
+      ++n_tcs_received;
+    } catch (const appfwk::QueueTimeoutExpired&) {
+      // It's fine if there was no new input
+    }
+
+    try {
+      dfmessages::DataRequest data_request;
+      m_input_queue_dr->pop(data_request);
+      popped_anything = true;
+      ++n_requests_received;
+      m_request_handler_impl->issue_request(data_request, *m_output_queue_frag);
+    } catch (const appfwk::QueueTimeoutExpired&) {
+      // It's fine if there was no new input
+    }
+
+    if (!popped_anything) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+  } // while (running_flag.load())
+
+  TLOG() << get_name() << " exiting do_work() method. Received " << n_tcs_received << " TCs " << " and " << n_requests_received << " data requests";
+}
+} // namespace trigger
+} // namespace dunedaq
+
+DEFINE_DUNE_DAQ_MODULE(dunedaq::trigger::TCBuffer)
---
+++ plugins/TCBuffer.hpp
@@ -0,0 +1,160 @@
+/**
+ * @file TCBuffer.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2021.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#ifndef TRIGGER_PLUGINS_TCBUFFER_HPP_
+#define TRIGGER_PLUGINS_TCBUFFER_HPP_
+
+#include "appfwk/DAQModule.hpp"
+#include "appfwk/DAQModuleHelper.hpp"
+#include "appfwk/DAQSink.hpp"
+#include "appfwk/DAQSource.hpp"
+#include "daqdataformats/Fragment.hpp"
+#include "readoutlibs/FrameErrorRegistry.hpp"
+#include "readoutlibs/models/DefaultRequestHandlerModel.hpp"
+#include "readoutlibs/models/BinarySearchQueueModel.hpp"
+#include "triggeralgs/TriggerActivity.hpp"
+#include "triggeralgs/TriggerObjectOverlay.hpp"
+#include "utilities/WorkerThread.hpp"
+
+#include "trigger/Issues.hpp"
+#include "triggeralgs/TriggerCandidate.hpp"
+
+#include <chrono>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+
+namespace dunedaq {
+namespace trigger {
+class TCBuffer : public dunedaq::appfwk::DAQModule
+{
+public:
+  explicit TCBuffer(const std::string& name);
+
+  TCBuffer(const TCBuffer&) = delete;
+  TCBuffer& operator=(const TCBuffer&) = delete;
+  TCBuffer(TCBuffer&&) = delete;
+  TCBuffer& operator=(TCBuffer&&) = delete;
+
+  void init(const nlohmann::json& iniobj) override;
+  void get_info(opmonlib::InfoCollector& ci, int level) override;
+
+private:
+
+  struct TCWrapper
+  {
+    triggeralgs::TriggerCandidate candidate;
+    std::vector<uint8_t> candidate_overlay_buffer;
+    // Don't really want this default ctor, but IterableQueueModel requires it
+    TCWrapper() {}
+    
+    TCWrapper(triggeralgs::TriggerCandidate c)
+      : candidate(c)
+    {
+      populate_buffer();
+    }
+
+    void populate_buffer()
+    {
+      candidate_overlay_buffer.resize(triggeralgs::get_overlay_nbytes(candidate));
+      triggeralgs::write_overlay(candidate, candidate_overlay_buffer.data());
+    }
+    
+    // comparable based on first timestamp
+    bool operator<(const TCWrapper& other) const
+    {
+      return this->candidate.time_start < other.candidate.time_start;
+    }
+
+    uint64_t get_first_timestamp() const // NOLINT(build/unsigned)
+    {
+      return candidate.time_start;
+    }
+
+    void set_first_timestamp(uint64_t ts) // NOLINT(build/unsigned)
+    {
+      candidate.time_start = ts;
+    }
+
+    uint64_t get_timestamp() const // NOLINT(build/unsigned)
+    {
+      return candidate.time_start;
+    }
+
+
+    size_t get_payload_size() { return candidate_overlay_buffer.size(); }
+
+    size_t get_num_frames() { return 1; }
+
+    size_t get_frame_size() { return get_payload_size(); }
+
+    uint8_t* begin()
+    {
+      return candidate_overlay_buffer.data();
+    }
+    
+    uint8_t* end()
+    {
+      return candidate_overlay_buffer.data()+candidate_overlay_buffer.size();
+    }
+
+    //static const constexpr size_t fixed_payload_size = 5568;
+    static const constexpr daqdataformats::GeoID::SystemType system_type = daqdataformats::GeoID::SystemType::kDataSelection;
+    static const constexpr daqdataformats::FragmentType fragment_type = daqdataformats::FragmentType::kTriggerCandidates;
+    // No idea what this should really be set to
+    static const constexpr uint64_t expected_tick_difference = 16; // NOLINT(build/unsigned)
+
+};
+
+  void do_conf(const nlohmann::json& config);
+  void do_start(const nlohmann::json& obj);
+  void do_stop(const nlohmann::json& obj);
+  void do_scrap(const nlohmann::json& obj);
+  void do_work(std::atomic<bool>&);
+
+  dunedaq::utilities::WorkerThread m_thread;
+
+  using tcs_source_t = dunedaq::appfwk::DAQSource<triggeralgs::TriggerCandidate>;
+  std::unique_ptr<tcs_source_t> m_input_queue_tcs{nullptr};
+
+  using dr_source_t = dunedaq::appfwk::DAQSource<dfmessages::DataRequest>;
+  std::unique_ptr<dr_source_t> m_input_queue_dr{nullptr};
+
+  using fragment_sink_t = dunedaq::appfwk::DAQSink<std::pair<std::unique_ptr<daqdataformats::Fragment>, std::string>>;
+  std::unique_ptr<fragment_sink_t> m_output_queue_frag{nullptr};
+
+  std::chrono::milliseconds m_queue_timeout;
+
+  using buffer_object_t = TCWrapper;
+  using latency_buffer_t = readoutlibs::BinarySearchQueueModel<buffer_object_t>;
+  std::unique_ptr<latency_buffer_t> m_latency_buffer_impl{nullptr};
+  using request_handler_t = readoutlibs::DefaultRequestHandlerModel<buffer_object_t, latency_buffer_t>;
+  std::unique_ptr<request_handler_t> m_request_handler_impl{nullptr};
+
+  // Don't actually use this, but it's currently needed as arg to request handler ctor
+  std::unique_ptr<readoutlibs::FrameErrorRegistry> m_error_registry;
+};
+} // namespace trigger
+} // namespace dunedaq
+
+namespace dunedaq {
+namespace readoutlibs {
+
+template<>
+uint64_t
+get_frame_iterator_timestamp(uint8_t* it)
+{
+  detdataformats::trigger::TriggerActivity* activity = reinterpret_cast<detdataformats::trigger::TriggerActivity*>(it);
+  return activity->data.time_start;
+}
+
+}
+}
+
+#endif // TRIGGER_PLUGINS_TCBUFFER_HPP_
