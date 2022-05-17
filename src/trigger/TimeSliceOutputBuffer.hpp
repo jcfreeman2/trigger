faa18d9f0c0d3825d0cfe3fc1bfbba38e9766ada
Philip Rodrigues <39594854+philiprodrigues@users.noreply.github.com>
2022-05-17 14:55:54 +0100

    Correctly handle the outputting of heartbeat Sets in GenericMaker (#116)
    We were previously sending out heartbeat Set<T>s as soon as we received
    them, but that was wrong, because the output buffer was potentially
    holding Ts with times earlier than the heartbeat's time. Those Ts were
    then output later than the heartbeat, breaking the ordering guarantee.
    
    So now, TimeSliceOutputBuffer holds _two_ buffers: the buffer of
    Ts (which it had before) and a new buffer of heartbeat
    Set<T>s. Heartbeat Set<T>s are added via the buffer_heartbeat()
    function. Adding a heartbeat to the buffer advances the latest seen
    time, which is used to determine what data is ready to be sent
    out. When flush() is called, the return (by reference) value is the
    next heartbeat Set<T> if it sits at the start of the current time
    window; otherwise it is a payload Set<T> with the Ts that fall in the
    current window.

--- src/trigger/TimeSliceOutputBuffer.hpp
+++ src/trigger/TimeSliceOutputBuffer.hpp
@@ -27,6 +27,13 @@ struct time_start_greater_t
   bool operator()(const T& a, const T& b) { return a.time_start > b.time_start; }
 };
 
+// TODO Philip Rodrigues rodriges@fnal.gov 2022-04-27: Same comment as above, plus we should really fix the time_start/start_time inconsistency
+template<class T>
+struct start_time_greater_t
+{
+  bool operator()(const T& a, const T& b) { return a.start_time > b.start_time; }
+};
+
 // When writing Set<T> to a queue, we want to buffer all T with the same for
 // some time, to ensure that Set<T> are generated with all T from that window,
 // assuming that the T may be generated in some arbitrary, but not too tardy,
@@ -70,6 +77,29 @@ public:
     }
   }
 
+  // Add a new heartbeat Set to the buffer
+  void buffer_heartbeat(const Set<T> heartbeat)
+  {
+    if (m_next_window_start == 0) {
+      // Window start time is unknown. pick it as the window that contains the
+      // first element of in. Window start time must be multiples of m_window_time
+      m_next_window_start = heartbeat.start_time;
+    }
+
+    if (heartbeat.start_time < m_next_window_start) {
+      ers::warning(TardyOutputError(ERS_HERE, m_name, m_algorithm, heartbeat.start_time, m_next_window_start));
+      // heartbeat is discarded
+    } else if (heartbeat.start_time % m_window_time != 0) {
+      ers::warning(UnalignedHeartbeat(ERS_HERE, m_name, m_algorithm, heartbeat.start_time, m_window_time));
+      // heartbeat is discarded
+    } else {
+      m_heartbeat_buffer.push(heartbeat);
+      if (m_largest_time < heartbeat.start_time) {
+        m_largest_time = heartbeat.start_time;
+      }
+    }
+  }
+
   void reset() { m_next_window_start = 0; }
 
   void set_window_time(const daqdataformats::timestamp_t window_time)
@@ -93,30 +123,52 @@ public:
     }
   }
 
-  bool empty() { return m_buffer.empty(); }
+  bool empty() { return m_buffer.empty() && m_heartbeat_buffer.empty(); }
 
-  // Fills time_slice, start_time, and end_time with the contents of the buffer
-  // that fall within the first window. This removes the contents that are added
-  // to time_slice from the buffer, and moves to the next window. Call when
-  // ready() is true for full windows, or whenever to drain this buffer.
-  void flush(std::vector<T>& time_slice, daqdataformats::timestamp_t& start_time, daqdataformats::timestamp_t& end_time)
+  // Fills out_set with the contents of the buffer that fall within
+  // the first window, or with the next buffered heartbeat Set, if it
+  // should be output next . This removes the contents that are added
+  // to out_set from the buffer, and moves to the next window. Call
+  // when ready() is true for full windows, or whenever to drain this
+  // buffer.
+  void flush(Set<T>& out_set)
   {
-    start_time = m_next_window_start;
-    end_time = m_next_window_start + m_window_time;
+    // Heartbeats have no duration and live at window boundaries. If
+    // there's a heartbeat at the start of our time window, we send it
+    // out and don't advance the window: there might be objects in the
+    // window that we want. We'll get those next time we call flush(),
+    // because the heartbeat will have been popped from the heartbeat
+    // buffer
+    if (!m_heartbeat_buffer.empty() && m_heartbeat_buffer.top().start_time == m_next_window_start) {
+      auto& hb = m_heartbeat_buffer.top();
+      TLOG_DEBUG(4) << "Flushing heartbeat with start time " << hb.start_time;
+      out_set.start_time = hb.start_time;
+      out_set.end_time = hb.end_time;
+      out_set.origin = hb.origin;
+      out_set.type = Set<T>::Type::kHeartbeat;
+      m_heartbeat_buffer.pop();
+      return;
+    }
+
+    out_set.type = Set<T>::Type::kPayload;
+    out_set.start_time = m_next_window_start;
+    out_set.end_time = m_next_window_start + m_window_time;
     m_next_window_start = m_next_window_start + m_window_time;
-    while (!m_buffer.empty() && m_buffer.top().time_start <= end_time) {
-      if (m_buffer.top().time_start < start_time) {
+    while (!m_buffer.empty() && m_buffer.top().time_start <= out_set.end_time) {
+      if (m_buffer.top().time_start < out_set.start_time) {
         ers::warning(WindowlessOutputError(ERS_HERE, m_name, m_algorithm));
         // top is discarded
       } else {
-        time_slice.emplace_back(m_buffer.top());
+        out_set.objects.emplace_back(m_buffer.top());
       }
       m_buffer.pop();
     }
+    TLOG_DEBUG(4) << "Filled payload from " << out_set.start_time << " to " << out_set.end_time << " with " << out_set.objects.size() << " objects";
   }
 
 private:
   std::priority_queue<T, std::vector<T>, time_start_greater_t<T>> m_buffer;
+  std::priority_queue<Set<T>, std::vector<Set<T>>, start_time_greater_t<Set<T>>> m_heartbeat_buffer;
   const std::string &m_name, &m_algorithm;
   daqdataformats::timestamp_t m_next_window_start; // tick start of next window, or 0 if not yet known
   daqdataformats::timestamp_t m_buffer_time;       // ticks to buffer after a window before a window is valid
