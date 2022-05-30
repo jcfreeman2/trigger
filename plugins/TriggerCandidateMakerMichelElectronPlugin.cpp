75fff1d0e0d10a416d24e072e2f7da159a0f2a6e
Charlie Batchelor <cbatchel@lxplus780.cern.ch>
2022-05-30 18:04:50 +0200

    Files to start a michel trigger algorithm.

---
+++ plugins/TriggerCandidateMakerMichelElectronPlugin.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file TriggerCandidateMakerMichelElectron.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "trigger/AlgorithmPlugins.hpp"
+#include "triggeralgs/MichelElectron/TriggerCandidateMakerMichelElectron.hpp"
+
+DEFINE_DUNE_TC_MAKER(triggeralgs::TriggerCandidateMakerMichelElectron)
